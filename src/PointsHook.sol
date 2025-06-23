21a65b6e629aabd07fa5e62d20e964d42e36548a
VictoriaAde <adedayovicky123@gmail.com>
2025-06-23 20:10:37 +0100

    test for points hook

--- src/PointsHook.sol
+++ src/PointsHook.sol
@@ -1,47 +1,53 @@
-// SPDX -License-Identifier: MIT
-pragma solidity ^0.8.25;
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
 import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
-import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
-import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {ERC1155} from "solmate/src/tokens/ERC1155.sol";
-import  {PoolId} from "v4-core/types/PoolId.sol";
-import {PoolKey} from "v4-core/types/PoolKey.sol"; 
-import {SwapParams} from "v4-core/types/PoolOperation.sol";
+
+import {Currency} from "v4-core/types/Currency.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {PoolId} from "v4-core/types/PoolId.sol";
 import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
+import {SwapParams, ModifyLiquidityParams} from "v4-core/types/PoolOperation.sol";
+
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+
+import {Hooks} from "v4-core/libraries/Hooks.sol";
 
 contract PointsHook is BaseHook, ERC1155 {
     constructor(IPoolManager _manager) BaseHook(_manager) {}
 
-    function getHooksPermissions() 
-    public
-    pure 
-    override
-    returns (Hooks.Permissions memory)
+    function getHookPermissions()
+        public
+        pure
+        override
+        returns (Hooks.Permissions memory)
     {
-      return 
-        Hooks.Permissions({
-            beforeInitialize: false,
-            afterInitialize: false,
-            beforeAddLiquidity: false,
-            afterAddLiquidity: false,
-            beforeRemoveLiquidity: false,
-            afterRemoveLiquidity: false,
-            beforeSwap: false,
-            afterSwap: true, //  afterSwap = true
-            beforeDonate: false,
-            afterDonate: false,
-            beforeSwapReturnDelta: false,
-            afterSwapReturnDelta: false,
-            afterAddLiquidityReturnDelta: false,
-            afterRemoveLiquidityReturnDelta: false
-        });
+        return
+            Hooks.Permissions({
+                beforeInitialize: false,
+                afterInitialize: false,
+                beforeAddLiquidity: false,
+                beforeRemoveLiquidity: false,
+                afterAddLiquidity: false,
+                afterRemoveLiquidity: false,
+                beforeSwap: false,
+                afterSwap: true,
+                beforeDonate: false,
+                afterDonate: false,
+                beforeSwapReturnDelta: false,
+                afterSwapReturnDelta: false,
+                afterAddLiquidityReturnDelta: false,
+                afterRemoveLiquidityReturnDelta: false
+            });
     }
 
-    function uri (uint256) public view override returns (string memory) {
-        return "";
+    function uri(uint256) public view virtual override returns (string memory) {
+        return "https://api.example.com/token/{id}";
     }
 
-  function _afterSwap(
+    function _afterSwap(
         address,
         PoolKey calldata key,
         SwapParams calldata swapParams,
@@ -72,8 +78,7 @@ contract PointsHook is BaseHook, ERC1155 {
         return (this.afterSwap.selector, 0);
     }
 
-
-   function _assignPoints(
+    function _assignPoints(
         PoolId poolId,
         bytes calldata hookData,
         uint256 points
